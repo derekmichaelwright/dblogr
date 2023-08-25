000fe0814e885e95d29c2a10fea5d0837a843e1f
derekmichaelwright <derek.wright@usask.ca>
2023-08-25 18:28:40 -0500

    edit

---
+++ cv/mergepdfs.R
@@ -0,0 +1,4 @@
+
+setwd("C:/gitfolder/dblogr/cv/")
+qpdf::pdf_combine(input = c("lentil_cover.pdf", "cv.derek.m.wright.pdf"),
+                  output = "Lentil_Faba_Breeder_Derek_Wright.pdf")
---
+++ cv/transcripts.R
@@ -0,0 +1,70 @@
+Courses Taken at Justus-Liebig Universität
+
+:::concise
+- MK 02 Biostatistics and Experimental Design
+- MK 05 Plant Protection and Bioengeneering
+- MK 11 Special Biochemistry II
+- MK 13 Risk Assessment, Bio-safety and Patent Law
+- MK 16 Biotechnology and Genomics
+- MK 18 Microbial-Food-Biotechnology
+- MK 57 Molecular Phytopathology
+- MP 20 Plant Breeding: Special Topics of Resistance and Quality
+- MP 29 Plant Microbe Interactions
+- MP 76 Laboratory Course: Tissue Culturing and Genetic Transformation
+- MP 77 Laboratory Course: Plant Pathogens and Symbionts
+- MP 90 Molecular Entomology
+- MP 97 Microbial Diagnostics
+- MP 98 Molecular Plant Breeding
+- MP 100 Applied Statistics and Bioinformatics
+:::
+
+  Undergraduate Courses Taken at the University of Regina
+
+:::concise
+
+
+PHYS 109 General Physics I
+PHYS 119 General Physics II
+
+CHEM 102 Chemistry
+CHEM 240 Organic Chemistry I
+CHEM 241 Organic Chemistry II
+
+BIOC 220 Bioorganic Chemistry
+
+BIOL 100 Introductory Biology I
+BIOL 101 Introductory Biology II
+BIOL 205 Introductory Genetics
+BIOL 220 Introductory Microbiology
+BIOL 265 Animal Physiology
+BIOL 266 Plant Physiology
+BIOL 275 Ecology
+BIOL 276 Environmental Biology
+BIOL 288 Cell Biology
+BIOL 305 Genetics
+BIOL 335 Limnology
+BIOL 341 Biometrics
+BIOL 366 Advanced Plant Physiology
+BIOL 367 Plant Taxonomy
+BIOL 385 Vertebrate Animal Biology
+BIOL 396 Independent Research
+BIOL 405 Molecular Genetics
+BIOL 410 Bacterial Genetics
+BIOL 456 Global Biogeochemistry
+BIOL 485 Ornithology
+BIOL 488 Seminars in Biology
+BIOL 490BA Stable Isotope Ecology
+BIOL 490BF Environmental Microbiology
+BIOL 490BG Genomics
+BIOL 490BM Research in Microbiology I
+GEOG 120 Human Geography
+GEOG 329 Soil Geography
+MATH 110 Calculus I
+MATH 111 Calculus II
+STAT 151 Statistical Methods I
+CS 110 Prog & Problem Solving
+ENGL 100 Critical Reading and Writing I
+ENGL 110 Critical Reading & Writing II
+RLST 100 Intro to Religious Studies
+RLST 273 Religion & Science
+:::
