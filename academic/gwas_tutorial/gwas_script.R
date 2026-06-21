22a86f5c39d6bbffaa06daa3658caf86489eb439
derekmichaelwright <derek.wright@usask.ca>
2026-06-21 15:33:38 -0500

    edit

--- academic/gwas_tutorial/gwas_script.R
+++ academic/gwas_tutorial/gwas_script.R
@@ -3,18 +3,22 @@ install.packages("devtools")
 devtools::install_github("jiabowang/GAPIT")
 #
 myRootFolder <- "C:/gitfolder/dblogr/academic/gwas_tutorial/"
+myRootFolder <- "C:/Users/wrigh/OneDrive - University of Saskatchewan/gitfolder/dblogr/academic/gwas_tutorial/"
 #
 setwd(myRootFolder)
 #
 # Read in phenotype data
-myY <- read.csv("Data/myY.csv")
+myY <- read.csv("Data/myY.csv")[,-2]
 # Read in genotype data (note: header = F)
 myG <- read.csv("Data/myG_hmp.csv", header = F)
 #
 library(GAPIT)
 setwd( paste0(myRootFolder, "New_GWAS_Results") )
 # Simplest GWAS
-myGAPIT <- GAPIT( Y = myY, G = myG )
+myGAPIT <- GAPIT( Y = myY[,c(1:2)], G = myG,
+                  PCA.total = 4,
+                  model = c("MLM","MLMM","FarmCPU","BLINK"),
+                  Random.model = F, Phenotype.View = F )
 #
 #
 myGAPIT <- GAPIT( Y = myY[,-2], G = myG, model = c("MLM", "BLINK") )
